3c8ae3fa92d22ab40257b6c405d894c09cbcb359
Prahalad Belavadi <test@ALIPL0232.local>
2020-03-25 20:52:52 +0530

    add import functionality

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/new", (req, res) => {
-
   // return Address and private key from new key pair
 
   const keyPair = bitcoin.ECPair.makeRandom();
@@ -18,9 +17,17 @@ app.get("/new", (req, res) => {
   res.json({ private_key: keyPair.toWIF(), address });
 });
 
+app.get("/import", (req, res) => {
+  if (req.query.req) {
+    const wif = req.query.wif;
+    const keyPair = bitcoin.ECPair.fromWIF(wif);
+    const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
 
-
-
+    res.json({ address, private_key: wif });
+  }
+  res.status(401).send("Please supply WIF in query params");
+  // res.json({ body: req.body, params: req.params, query: req.query });
+});
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
