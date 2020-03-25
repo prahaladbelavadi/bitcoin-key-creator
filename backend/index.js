4a361893f25010d0760882257c5bcf0b51f35de3
Prahalad Belavadi <test@ALIPL0232.local>
2020-03-25 20:30:07 +0530

    create first endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
-const exporess = require("express");
+const express = require("express");
 const app = new express();
+const bitcoin = require("bitcoinjs-lib");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello crypto World!");
+});
+
+app.get("/new", (req, res) => {
+
+  // return Address and private key from new key pair
+
+  const keyPair = bitcoin.ECPair.makeRandom();
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+
+  res.json({ private_key: keyPair.toWIF(), address });
+});
+
+
+
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
